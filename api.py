c386a9b3bba04a6caab5520167d0e99e7433f83e
samar ben amar <samar.benamar.stg@sifast.com>
2022-11-28 16:48:07 +0100

    2nd commit

--- api.py
+++ api.py
@@ -20,6 +20,6 @@ try:
   response = session.get(url, params=parameters)
   data = json.loads(response.text)
   print(data)
-  print("Hello Github Action Fouth TRY")
+  print("Hello Github Action Fifth TRY")
 except (ConnectionError, Timeout, TooManyRedirects) as e:
   print(e)
